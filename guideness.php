ae93b794c274818d3c114d6dea712815325f2124
Woon Yong Ming <woonyongming0930@e.newera.edu.my>
2024-01-04 16:02:20 +0800

    css combine

--- guideness.php
+++ guideness.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="guideness.css">
     <link rel="stylesheet" href="guideness.js">
-    <link rel="stylesheet" href="styles2.css">
+    <link rel="stylesheet" href="styles.css">
 
     <title>Guideness</title>
 </head>
