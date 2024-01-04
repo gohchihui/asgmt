69334cfcaba88c33ce7cccad12638caf4f48504e
REINGNL <131546698+REINGNL@users.noreply.github.com>
2024-01-04 11:27:55 +0800

    Add files via upload

---
+++ guideness.js
@@ -0,0 +1,53 @@
+document.getElementById('toggleButton').addEventListener('click', function() {
+    var content = document.getElementById('content');
+    content.classList.toggle('hidden');
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var scrollButton = document.getElementById("scrollButton");
+
+    window.onscroll = function () {
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        scrollButton.style.display = "block";
+      } else {
+        scrollButton.style.display = "none";
+      }
+    };
+    scrollButton.addEventListener("click", function () {
+      document.body.scrollTop = 0; 
+      document.documentElement.scrollTop = 0;
+    });
+  });
+
+
+  function toggleContent(showId, hideId) {
+    var showContent = document.getElementById(showId);
+    var hideContent = document.getElementById(hideId);
+
+    if (showContent && hideContent) {
+      if (showContent.style.display === 'block') {
+      
+        showContent.style.display = 'none';
+      } else {
+        
+        showContent.style.display = 'block';
+        hideContent.style.display = 'none';
+      }
+    }
+  }
+
+  function openLinkInNewWindow(url) {
+    window.open(url, '_blank');
+  }
+
+  function toggleNavbar() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
